c40664b1cef4ff79a51cd91fb18f5c5e908c5309
Bondzik-S <bondziks@gmail.com>
2024-11-18 22:16:18 +0200

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 from taxi.models import Driver, Car, Manufacturer
 
@@ -6,10 +7,43 @@ from taxi.models import Driver, Car, Manufacturer
 def index(request):
     """View function for the home page of the site."""
 
-    context = {
+    context_index = {
         "num_drivers": Driver.objects.count(),
         "num_cars": Car.objects.count(),
         "num_manufacturers": Manufacturer.objects.count(),
     }
 
-    return render(request, "taxi/index.html", context=context)
+    return render(request, "taxi/index.html", context_index)
+
+
+class ManufacturerListView(generic.ListView):
+    model = Manufacturer
+    queryset = Manufacturer.objects.order_by("name")
+    template_name = "taxi/manufacturer_list.html"
+    context_object_name = "manufacturer_list"
+    paginate_by = 5
+
+
+class CarListView(generic.ListView):
+    model = Car
+    queryset = (Car.objects.select_related("manufacturer")
+                .prefetch_related("drivers"))
+    template_name = "taxi/car_list.html"
+    context_object_name = "car_list"
+    paginate_by = 5
+
+
+class CarDetailView(generic.DetailView):
+    model = Car
+
+
+class DriverListView(generic.ListView):
+    model = Driver
+    paginate_by = 5
+
+
+class DriverDetailView(generic.DetailView):
+    model = Driver
+    queryset = Driver.objects.all()
+    template_name = "taxi/driver_detail.html"
+    context_object_name = "driver_details"
